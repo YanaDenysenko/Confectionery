61efb8e315d7e871f5c17ca0767b03f056e9bac6
YanaDenysenko <yana.denysenko52@gmail.com>
2021-04-30 14:26:36 +0300

    Added UnitOfWork to DAL

---
+++ Confectionery/DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Entities;
+using DAL.Repositories.Impl;
+using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace DAL.UnitOfWork { 
+    public interface IUnitOfWork : IDisposable 
+    { 
+        GoodsRepository Goodss { get; } 
+        StockRepository Stocks { get; }
+        CategoriesRepository Categoriess { get; }
+        void Save(); 
+    } 
+}
