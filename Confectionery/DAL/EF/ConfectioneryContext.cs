0da4057deacefb907f8f205198f9b5f8e5fb4bc1
YanaDenysenko <yana.denysenko52@gmail.com>
2021-04-30 13:58:17 +0300

    Update ConfectioneryContext.cs

--- Confectionery/DAL/EF/ConfectioneryContext.cs
+++ Confectionery/DAL/EF/ConfectioneryContext.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using DAL.Entities;
 
 namespace DAL.EF
 {
     public class ConfectioneryContext : DbContext
     {
+        public DbSet<Entities.Stock> Stocks { get; set; }
+        public DbSet<Entities.Goods> Goodss { get; set; }
+        public DbSet<Entities.Categories> Categoriess { get; set; }
         public ConfectioneryContext(DbContextOptions options) : base(options)
         {
 
