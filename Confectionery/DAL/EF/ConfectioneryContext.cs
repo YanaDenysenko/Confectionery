1a5f9575a845949ff187b0b9184e0e092f6aeb7a
YanaDenysenko <yana.denysenko52@gmail.com>
2021-04-30 13:34:26 +0300

    Added EF to DAL

---
+++ Confectionery/DAL/EF/ConfectioneryContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.EF
+{
+    public class ConfectioneryContext : DbContext
+    {
+        public ConfectioneryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
