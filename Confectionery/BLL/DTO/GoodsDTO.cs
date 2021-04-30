0bee81a1adc40862988db8caac1cccff25e9a835
YanaDenysenko <yana.denysenko52@gmail.com>
2021-05-01 00:11:18 +0300

    Create DTO

---
+++ Confectionery/BLL/DTO/GoodsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.DTO
+{
+    public class GoodsDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategorieId { get; set; }
+        public int ConfectionerId { get; set; }
+    }
+}
---
+++ Confectionery/BLL/DTO/StockDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.DTO
+{
+    public class StockDTO
+    {
+        public int VendorCose { get; set; }
+        public int GoodId { get; set; }
+        public int Amount { get; set; }
+        public int Price { get; set; }
+    }
+}
